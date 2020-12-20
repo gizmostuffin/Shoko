77bd982a4790a538d75487a58de94fb620306862
anil chauhan <anilchauhanxda@gmail.com>
2020-12-20 11:45:11 +0530

    Switched to google_trans_new

--- Shoko/modules/translator.py
+++ Shoko/modules/translator.py
@@ -1,102 +1,97 @@
-from typing import Optional, List
-from gtts import gTTS
-import os
-import requests
-import json
 from emoji import UNICODE_EMOJI
-
-from telegram import ChatAction
+from google_trans_new import LANGUAGES, google_translator
+from telegram import ParseMode, Update
 from telegram.ext import run_async
 
 from Shoko import dispatcher
 from Shoko.modules.disable import DisableAbleCommandHandler
-from Shoko.modules.helper_funcs.alternate import typing_action, send_action
-
-from googletrans import Translator
 
 
 @run_async
-@typing_action
-def gtrans(update, context):
-    msg = update.effective_message
-    args = context.args
-    lang = " ".join(args)
-    if not lang:
-        lang = "en"
-    translate_text = msg.reply_to_message.text
-    ignore_text = UNICODE_EMOJI.keys()
-    for emoji in ignore_text:
-        if emoji in translate_text:
-            translate_text = translate_text.replace(emoji, "")
+def totranslate(update, context):
+    message = update.effective_message
+    problem_lang_code = []
+    for key in LANGUAGES:
+        if "-" in key:
+            problem_lang_code.append(key)
 
-    translator = Translator()
     try:
-        translated = translator.translate(translate_text, dest=lang)
-        trl = translated.src
-        results = translated.text
-        msg.reply_text("Translated from {} to {}.\n {}".format(trl, lang, results))
-    except:
-        msg.reply_text("Error! invalid language code.")
+        if message.reply_to_message:
+            args = update.effective_message.text.split(None, 1)
+            if message.reply_to_message.text:
+                text = message.reply_to_message.text
+            elif message.reply_to_message.caption:
+                text = message.reply_to_message.caption
 
+            try:
+                source_lang = args[1].split(None, 1)[0]
+            except (IndexError, AttributeError):
+                source_lang = "en"
 
-@run_async
-@send_action(ChatAction.RECORD_AUDIO)
-def gtts(update, context):
-    msg = update.effective_message
-    reply = " ".join(context.args)
-    if not reply:
-        if msg.reply_to_message:
-            reply = msg.reply_to_message.text
         else:
-            return msg.reply_text(
-                "Reply to some message or enter some text to convert it into audio format!"
-            )
-        for x in "\n":
-            reply = reply.replace(x, "")
-    try:
-        tts = gTTS(reply)
-        tts.save("Shoko.mp3")
-        with open("Shoko.mp3", "rb") as speech:
-            msg.reply_audio(speech)
-    finally:
-        if os.path.isfile("Shoko.mp3"):
-            os.remove("Shoko.mp3")
+            args = update.effective_message.text.split(None, 2)
+            text = args[2]
+            source_lang = args[1]
 
+        if source_lang.count('-') == 2:
+            for lang in problem_lang_code:
+                if lang in source_lang:
+                    if source_lang.startswith(lang):
+                        dest_lang = source_lang.rsplit("-", 1)[1]
+                        source_lang = source_lang.rsplit("-", 1)[0]
+                    else:
+                        dest_lang = source_lang.split("-", 1)[1]
+                        source_lang = source_lang.split("-", 1)[0]
+        elif source_lang.count('-') == 1:
+            for lang in problem_lang_code:
+                if lang in source_lang:
+                    dest_lang = source_lang
+                    source_lang = None
+                    break
+            if dest_lang is None:
+                dest_lang = source_lang.split("-")[1]
+                source_lang = source_lang.split("-")[0]
+        else:
+            dest_lang = source_lang
+            source_lang = None
 
-# Open API key
-API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
-URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+        exclude_list = UNICODE_EMOJI.keys()
+        for emoji in exclude_list:
+            if emoji in text:
+                text = text.replace(emoji, '')
 
+        trl = google_translator()
+        if source_lang is None:
+            detection = trl.detect(text)
+            trans_str = trl.translate(text, lang_tgt=dest_lang)
+            return message.reply_text(
+                f"Translated from `{detection[0]}` to `{dest_lang}`:\n`{trans_str}`",
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            trans_str = trl.translate(
+                text, lang_tgt=dest_lang, lang_src=source_lang)
+            message.reply_text(
+                f"Translated from `{source_lang}` to `{dest_lang}`:\n`{trans_str}`",
+                parse_mode=ParseMode.MARKDOWN)
 
-@run_async
-@typing_action
-def spellcheck(update, context):
-    if update.effective_message.reply_to_message:
-        msg = update.effective_message.reply_to_message
-
-        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
-
-        res = requests.get(URL, params=params)
-        changes = json.loads(res.text).get("LightGingerTheTextResult")
-        curr_string = ""
-        prev_end = 0
-
-        for change in changes:
-            start = change.get("From")
-            end = change.get("To") + 1
-            suggestions = change.get("Suggestions")
-            if suggestions:
-                sugg_str = suggestions[0].get("Text")  # should look at this list more
-                curr_string += msg.text[prev_end:start] + sugg_str
-                prev_end = end
-
-        curr_string += msg.text[prev_end:]
-        update.effective_message.reply_text(curr_string)
-    else:
+    except IndexError:
         update.effective_message.reply_text(
-            "Reply to some message to get grammar corrected text!"
-        )
+            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
+            "Example: `/tr en-hi` to translate from English to Hindi\n"
+            "Or use: `/tr hi` for automatic detection and translating it into Hindi.\n"
+            "See [List of Language Codes](https://t.me/spookyanii/91) for a list of language codes.",
+            parse_mode="markdown",
+            disable_web_page_preview=True)
+    except ValueError:
+        update.effective_message.reply_text(
+            "The intended language is not found!")
+    else:
+        return
 
-dispatcher.add_handler(DisableAbleCommandHandler(["tr", "tl"], gtrans, pass_args=True))
-dispatcher.add_handler(DisableAbleCommandHandler("tts", gtts, pass_args=True))
-dispatcher.add_handler(DisableAbleCommandHandler("splcheck", spellcheck))
+
+__help__ = """
+- /tr or /tl (language code) as reply to a long message
+*Example:* 
+  `/tr en`*:* translates something to english
+  `/tr hi-en`*:* translates hindi to english
+"""
